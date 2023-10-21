918364a0f9796f02d2a849093bf1755f895ef2a3
Evgene-Kopylov <eugenevkopylov@gmail.com>
2023-10-21 04:36:54 +0200

    обновил алгоритм, чтобы можно было задавать несколько участков

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 import random
-from typing import List, Optional
+from typing import List, Optional, Union
+import builtins
 
 
 class Deck:
     def __init__(self, values: List[str], nominal: Optional[List[str]] = None):
         self.nominal = nominal or ['∆', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'σ', 'λ', '♛']
         self.values = values or ['☐', '○', '▲', 'Ω', '⌗']
-        self.chain = None
+        self.deck = None
         self.target_chain = None
         self.line = None
 
     def new_deck(self):
-        self.chain = []
+        self.deck = []
         for nominal in self.nominal:
             for value in self.values:
-                self.chain.append(f"{nominal}{value}")
-        return self.chain
+                self.deck.append(f"{nominal}{value}")
+        return self.deck
 
     def print_deck(self):
-        line = " ".join(self.chain)
+        line = " ".join(self.deck)
         print(line)
 
     def shuffle_deck(self):
-        random.shuffle(self.chain)
+        random.shuffle(self.deck)
 
     def take(self, n):
-        if n > len(self.chain):
+        if n > len(self.deck):
             raise ValueError("Not enough cards in the deck")
-        taken_cards = self.chain[:n]
-        self.chain = self.chain[n:]  # Remove the taken cards from the deck
+        taken_cards = self.deck[:n]
+        self.deck = self.deck[n:]  # Remove the taken cards from the deck
         return taken_cards
 
     def chain_check(self, chain: List, show: bool = False):
@@ -47,11 +48,21 @@ class Deck:
                     break
         return len(chain) == 2
 
-    def psv(self, target: List[str], before: int = 0, after: int = 0):
+    def collect_target_chain(self, target: List[Union[List[str], int]]) -> List[str]:
+        self.target_chain = []
+        for item in target:
+            match type(item):
+                case builtins.int:
+                    self.target_chain += self.take(item)
+                case builtins.list:
+                    self.target_chain += item
+        return self.target_chain
+
+    def psv(self, target: List[str]):
         for _ in range(10000):
             self.new_deck()
             self.shuffle_deck()
-            self.target_chain = self.take(before) + target + self.take(after)
+            self.target_chain = self.collect_target_chain(target)
             self.line = "  ".join(self.target_chain)
             assert len(self.target_chain) > 2, "Длинна целевой цепочки должна быть больше 2"
             if self.chain_check(self.target_chain):
@@ -62,45 +73,10 @@ class Deck:
 
 
 if __name__ == '__main__':
-    # deck = Deck(['▲', 'Ω', '丄'])
-    # chain.new_deck()  # Create the deck
-    # chain.print_deck()
-    # chain.shuffle_deck()  # Shuffle the deck
-    # chain.print_deck()  # Print the shuffled deck
-    # chain = Chain(['▲', 'Ω', '丄'])
-    # chain.new_deck()  # Create and initialize the deck
-    # chain.shuffle_deck()  # Shuffle the deck
-    #
-    # # n = 5  # Number of cards to take
-    # # taken_cards = chain.take(n)
-    # # print(f"Taken cards: {taken_cards}")
+    deck = Deck(
+        values=['△', '○', '☐', 'Ω'],
+        nominal=['∆', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'σ', 'λ', '♛']
+    )
 
-    # target = ['λ○', '8○']  # "Пап, дай денег"
-    # print(chain.psv(target=target, before=2, after=5))
-    # print(chain.target_chain)
-
-    # deck = Deck(['○', '♡', '⌗', 'Ω', '丄'])
-    #
-    # target = ['λ○', '3♡', 'σ⌗', '7♡', '4⌗']
-    # print(deck.psv(target=target, before=4, after=3))
-    # print(deck.target_chain)
-
-    # deck = Deck(['♡', '▲', '♧', '○'])
-    #
-    # target = ['8○', '6♡', '9♠', '4♧', '10○']
-    # print(deck.psv(target=target, before=3, after=4))
-    # print(deck.target_chain)
-
-    # deck = Deck(['▲', '○', '♧'])
-    #
-    # target = ['Δ▲', '8○', '4○', '8♧']
-    # print(deck.psv(target=target, before=3, after=2))
-    # print(deck.target_chain)
-
-    deck = Deck(['△', '○', '☐', 'Ω'])
-    
-    target = ['4△', '♛Ω', 'λ○']
-    print(deck.psv(target=target, before=3, after=2))
-    print(deck.target_chain)
-
-    print('💀')
+    target = [3, ['4△'], 2, ['♛Ω', 'λ○'], 3]
+    print(deck.psv(target=target))
