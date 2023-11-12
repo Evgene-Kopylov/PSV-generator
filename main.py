b539b8120576179e1a90928060159611806aa512
Evgene-Kopylov <eugenevkopylov@gmail.com>
2023-11-12 14:40:17 +0200

    создать колоду

---
+++ main.py
@@ -0,0 +1,42 @@
+import random
+from typing import List, Optional
+
+
+class Deck:
+    def __init__(self, values: List[str] = None, nominal: List[str] = None):
+        """
+        Конструктор класса Deck.
+
+        Parameters:
+        - values: список строк, представляющих значения карт.
+        - nominal: список строк, представляющих номиналы карт.
+
+        Attributes:
+        - nominal: список строк, представляющих номиналы карт.
+        - values: список строк, представляющих значения карт.
+        - deck: список карт в колоде.
+        """
+        self.nominal = nominal or ['∆', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'ß', 'λ', '♛']
+        self.values = values or ['☐', '○', '▲', 'Ω', '⌗']
+        self.deck = None
+
+    def new_deck(self):
+        """
+        Создает новую колоду карт, объединяя номиналы и значения.
+
+        Returns:
+        - deck: список карт в колоде.
+        """
+        self.deck = [f"{nominal}{value}" for nominal in self.nominal for value in self.values]
+        return self.deck
+
+    def print_deck(self):
+        """Выводит текущую колоду карт."""
+        line = " ".join(self.deck)
+        print(line)
+
+
+if __name__ == '__main__':
+    deck = Deck()
+    deck.new_deck()
+    deck.print_deck()
