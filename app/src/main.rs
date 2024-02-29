4b0ac01dbc4b4a9519effb6b602afd72c255c4f6
Evgene-Kopylov <evgenekopylov@gmail.com>
2024-02-29 19:23:44 +0200

    wip tg_bot

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use std::time::Instant;
 
 mod patience;
@@ -5,17 +7,34 @@ mod settings;
 mod test_patience;
 use crate::patience::{Deck, MySpread};
 
-fn main() {
-    let start = Instant::now();
-    let suits = vec!["☐", "L", "▲", "♡", "○"];
-    let nominals = vec![
-        "T", "2", "3", "4", "5", "6", "7", "8", "9", "10", "β", "λ", "♛",
-    ];
-    let deck = Deck::new(suits, nominals);
-    let target = vec!["4", "2○", "β☐", "2☐", "3○", "9"];
-    let mut my_spread = MySpread::new(deck);
-    my_spread.patience(target);
-    let duration = start.elapsed();
+// fn main() {
+//     let start = Instant::now();
+//     let suits = vec!["☐", "L", "▲", "♡", "○"];
+//     let nominals = vec![
+//         "T", "2", "3", "4", "5", "6", "7", "8", "9", "10", "β", "λ", "♛",
+//     ];
+//     let deck = Deck::new(suits, nominals);
+//     let target = vec!["4", "2○", "β☐", "2☐", "3○", "9"];
+//     let mut my_spread = MySpread::new(deck);
+//     my_spread.patience(target);
+//     let duration = start.elapsed();
 
-    println!("Time elapsed in expensive_function() is: {:?}", duration);
+//     println!("Time elapsed in expensive_function() is: {:?}", duration);
+// }
+
+
+use dotenv_codegen::dotenv;
+use teloxide::{requests::Requester, types::Message, Bot};
+
+#[tokio::main]
+async fn main() {
+    log::info!("Starting throw dice bot...");
+
+    let bot = Bot::new(dotenv!("TELOXIDE_TOKEN"));
+
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_dice(msg.chat.id).await?;
+        Ok(())
+    })
+    .await;
 }
