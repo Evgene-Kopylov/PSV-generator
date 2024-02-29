b7ec0e116c9e4d921abb157ab1d273e2350c6abe
Evgene-Kopylov <evgenekopylov@gmail.com>
2024-02-29 21:36:36 +0200

    dice + commands

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,9 +1,9 @@
-// #![allow(unused)]
+#![allow(unused)]
 
 use dotenv_codegen::dotenv;
-use teloxide::{requests::Requester, types::Message, Bot};
 use patience_lib::patience::MySpread;
 
+use teloxide::{prelude::*, utils::command::BotCommands};
 
 #[tokio::main]
 async fn main() {
@@ -11,9 +11,44 @@ async fn main() {
 
     let bot = Bot::new(dotenv!("TELOXIDE_TOKEN"));
 
-    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        bot.send_dice(msg.chat.id).await?;
-        Ok(())
-    })
-    .await;
+    Command::repl(bot, answer).await;
+}
+
+#[derive(BotCommands, Clone)]
+#[command(
+    rename_rule = "lowercase",
+    description = "These commands are supported:"
+)]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "handle a username.")]
+    Username(String),
+    #[command(description = "handle a username and an age.", parse_with = "split")]
+    UsernameAndAge { username: String, age: u8 },
+    #[command(description = "1D6")]
+    Dice,
+}
+
+async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Help => {
+            bot.send_message(msg.chat.id, Command::descriptions().to_string())
+                .await?
+        }
+        Command::Username(username) => {
+            bot.send_message(msg.chat.id, format!("Your username is @{username}."))
+                .await?
+        }
+        Command::UsernameAndAge { username, age } => {
+            bot.send_message(
+                msg.chat.id,
+                format!("Your username is @{username} and age is {age}."),
+            )
+            .await?
+        }
+        Command::Dice => bot.send_dice(msg.chat.id).await?,
+    };
+
+    Ok(())
 }
